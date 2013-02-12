753345550e505afc02258bf462f8323427558119
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-12 08:53:16 +0330

    Fix the parse error of the page-start.

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
 	$home = true;
-	
+	$description = "I&rsquo;m a Web-Designer and Computer Programmer who always seeks the best possible way to do his job. I have worked in WebDesign, Computer Programming, Robotics, and Electronics. My mission is to initiate a better way of incorporating for developing better computer programs.";
+	$keywords = "Sharpasand, Mohammad Ali, Web, Webdesign, Programmer, Robotics, Portfolio, Curriculum Vita, Resume";
+	 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/home-content.inc.php';
 	require_once	$include_path . 'template/page-end.inc.php';
