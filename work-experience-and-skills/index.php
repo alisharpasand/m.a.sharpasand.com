638c95d5e736abe26133b3cd259e42c851535f23
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-12 11:37:08 +0330

    Add meta description and keywords for work-experience and skills.

--- work-experience-and-skills/index.php
+++ work-experience-and-skills/index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
+    $description = "";
+    $keywords = "Sharpasand, Mohammad Ali, Web, Prgrammer, Developer, Robotics, Electronics, Eniac Technology Group, Matorang Inc., Mechatronics Research Laboratory (MRL), Jahan Pardazesh Alborz (JPA)";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
