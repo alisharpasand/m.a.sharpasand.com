7eabc5526a2d49283e961018982279f7e3c3b669
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-12 08:33:37 +0330

    Add meta description and keyword capablity.

--- include/template/page-start.inc.php
+++ include/template/page-start.inc.php
@@ -4,8 +4,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-<meta name="description" content="" />
-<meta name="keywords" content="" />
+<meta name="description" content="<?php @ echo $description; ?>" />
+<meta name="keywords" content="<?php @ echo $keywords; ?>" />
 
 <title>Mohammad Ali Sharpasand's personal website</title>
 <style type="text/css" media="screen">
