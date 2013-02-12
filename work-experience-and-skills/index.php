72f39239a92477116f259c8d42d1d523b4939c34
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-12 14:44:51 +0330

    Add meta description and keywords for work experience and skills.

--- work-experience-and-skills/index.php
+++ work-experience-and-skills/index.php
@@ -1,8 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
-    $description = "";
-    $keywords = "Sharpasand, Mohammad Ali, Web, Prgrammer, Developer, Robotics, Electronics, Eniac Technology Group, Matorang Inc., Mechatronics Research Laboratory (MRL), Jahan Pardazesh Alborz (JPA)";
+$description = "I&rsquo;m mainly a web and computer programmer. I know myself as a proficient C++, and PHP programmer. However there are several different areas that I have worked in, such as JavaScript, CSS, XHTML, Pascal, and Bash.";
+$keywords = "Sharpasand, Mohammad Ali, Web, Prgrammer, Developer, Robotics, Electronics, Eniac Technology Group, Matorang Inc., Mechatronics Research Laboratory (MRL), Jahan Pardazesh Alborz (JPA)";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
