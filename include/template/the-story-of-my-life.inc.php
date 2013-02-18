d4a0d207d5733c0f556f308f871613337112df98
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-18 19:18:54 +0330

    Fix a word that could make misunderstanding. (mission -> target)

--- include/template/the-story-of-my-life.inc.php
+++ include/template/the-story-of-my-life.inc.php
@@ -1,5 +1,5 @@
 	<div id="container" class="fields">
 		<h2>The Story of My Life</h2>
-		<p>Here, I want to say how I have lived and how I want to live in future.  My <strong>missions</strong>, my <strong>dreams</strong> and my <strong>opinios</strong> are going to be here.</p>
-		<p>However, right know, there is nothing here.  This doesn&rsquo;t mean that I don&rsquo;t have any missions, but I haven&rsquo;t written its story yet.  I applogize for not filling this page, and hope to do this soon.</p>
+		<p>Here, I want to say how I have lived and how I want to live in future.  My <strong>targets</strong>, my <strong>dreams</strong> and my <strong>opinios</strong> are going to be here.</p>
+		<p>However, right know, there is nothing here.  This doesn&rsquo;t mean that I don&rsquo;t have any targets, but I haven&rsquo;t written its story yet.  I applogize for not filling this page, and hope to do this soon.</p>
 	</div>
