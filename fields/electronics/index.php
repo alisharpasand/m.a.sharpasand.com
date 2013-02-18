3c0d80224ed4b778f03126b2ef3c44f18e71d278
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-18 18:54:43 +0330

    Add description and keywords for the fields/electronics.

--- fields/electronics/index.php
+++ fields/electronics/index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
+	$description = "Electronics is one of my fields of activity. I&rsquo;ve worked on different electrical circuits, however I&rsquo;m better on digital ones. I&rsquo;ve worked in photo voltaic systems, communication busses, etc.";
+	$keywords = "Sharpasand, Mohammad Ali, Electronics, Logical Circuits, Digital Elecronics, Computer Archictecture, Photo Voltaic";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
