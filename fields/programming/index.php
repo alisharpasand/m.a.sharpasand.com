b6d8a1c5b1925ea365b5d12a35ed168d5a792e42
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-18 16:54:00 +0330

    Add description and keywords for the fields/programming.

--- fields/programming/index.php
+++ fields/programming/index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
+	$description = "Nice to say, Computer Programming is the thing I&rsquo;ve been following. Although I&rsquo;ve worked with a variety of languages and platforms, I prefer and proficient in Linux programming with C++, as well as Python and PHP.";
+	$keywords = "Sharpasand, Mohammad Ali, Web, Programmer, C++, PHP, Linux, Python, Bash, Software Architect";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
