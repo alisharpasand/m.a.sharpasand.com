d2f6c6b4303fb24618f03413e618ca5fdc2317fb
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-18 17:07:27 +0330

    Add description and keywords for the fields/robotics.

--- fields/robotics/index.php
+++ fields/robotics/index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
+	$description = "Robotics is always tempting enough that don&rsquo;t let me give up with. I&rsquo;m currently contributing with Mechatronics Research Laboratory, the nation$rsquo;s biggest and most honored robotics laboratory, for in Standard Platform League of the RoboCup.";
+	$keywords = "Sharpasand, Mohammad Ali, Robotics, NAO, Biped, Mechatronics Research Laboratory, Standard Platfrom League, RoboCup, IranOpen, Control";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
