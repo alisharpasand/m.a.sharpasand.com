28a1b9f598a1261aefaae2c42a139eb54009f5bf
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-02-18 19:15:25 +0330

    Add description and keywords for the-story-of-my-life.

--- the-story-of-my-life/index.php
+++ the-story-of-my-life/index.php
@@ -1,6 +1,8 @@
 <?php
 
 	$include_path = $_SERVER['DOCUMENT_ROOT'] . '/include/';
+	$description = "Here is where I write what I need to say about my life. I&rsquo;ve had many stories that I feel I should say them somewhere. The most important ones are to be written here.";
+	$keywords = "Sharpasand, Mohammad Ali, Interests, Activities, Stories, Dreams, Plans, Opinions, Blog";
 
 	require_once	$include_path . 'template/page-start.inc.php';
 	require_once	$include_path . 'template/header.inc.php';
