d25c1053fd2b9007a4d041fe45f122a2ca0e0519
Mohammad Ali Sharpasand <ali.msh.sh@gmail.com>
2013-05-04 08:35:54 +0430

    Fix a typo in the story of my life.

--- include/template/the-story-of-my-life.inc.php
+++ include/template/the-story-of-my-life.inc.php
@@ -1,5 +1,5 @@
 	<div id="container" class="fields">
 		<h2>The Story of My Life</h2>
 		<p>Here, I want to say how I have lived and how I want to live in future.  My <strong>targets</strong>, my <strong>dreams</strong> and my <strong>opinios</strong> are going to be here.</p>
-		<p>However, right know, there is nothing here.  This doesn&rsquo;t mean that I don&rsquo;t have any targets, but I haven&rsquo;t written its story yet.  I applogize for not filling this page, and hope to do this soon.</p>
+		<p>However, right now, there is nothing here.  This doesn&rsquo;t mean that I don&rsquo;t have any targets, but I haven&rsquo;t written its story yet.  I applogize for not filling this page, and hope to do this soon.</p>
 	</div>
