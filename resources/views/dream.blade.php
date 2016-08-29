67926999f489c3d685177f86ae625450c9974177
Mohammad Ali Sharpasand <m.a@sharpasand.com>
2016-08-29 15:08:37 +0430

    Write dream contents.

--- resources/views/dream.blade.php
+++ resources/views/dream.blade.php
@@ -2,11 +2,7 @@
                     <img alt="The Map of Internet by Freedom" src="/image/internetmap.png" />
                     <a href="https://www.internetsociety.org/map/global-internet-report/#limits-on-content">Courtesy of map: Internet Society</a>
                 </div>
-                <p>Lorem ipsum dolor sit amet, no natum fierent vel, duo in nisl tamquam tibique. Usu ludus maluisset dissentias ad, vix vidisse qualisque voluptaria an. Magna option vel ea, an enim iudicabit nam. Movet disputationi id cum, sed oblique postulant signiferumque ei.
-
-Id eam quodsi diceret concludaturque, id ubique quaerendum mel. No ius populo iriure, ut dictas liberavisse est. Has ne sanctus eloquentiam. Harum nusquam quaestio id usu. His suas vero ne. Nam ut utinam accusamus, cum vitae aeterno fastidii in, ad mutat interpretaris has.
-
-Eu nec reque fabellas singulis, ea quo ubique ocurreret. Duis habemus consequat nec id, vide dicam verterem te qui. Impedit eligendi pri ne, qui ornatus nonumes dissentiet ei. Ad erat maiorum mea, ad cibo noluisse pri. Pro vero malorum id. At per tractatos interesset, an mazim aperiam nominavi sed.
-
-Cum ex harum offendit democritum. Vim assum urbanitas tincidunt id, eos at quot possim fabellas. Ea sea falli dolorem maiorum, eum ipsum prodesset constituam te, nam vide diceret imperdiet te.</p>
+                <div class="text">
+                    <p><em>Internet</em> stands as the symbol of freedom for me and it represents human maturity. This priceless communication media, however, is not as liberal as possible yet. There are still limitations and impediments to it. Since I come from where there are the most content limitations on the internet, I understand and feel how one can impose a lot of problems to it. Observing the Internet moving many people out of darkness of imposition and isolation around the world beside the map of its freedom (the above map) shapes my dream: <blockquote>&ldquo;The world where everyone can access and help develop a connected, borderless, limitless Internet that creates opportunity and progress for all.&rdquo;</blockquote><span class="cite">&mdash;Internet Society</span></p>
+                </div>
  
