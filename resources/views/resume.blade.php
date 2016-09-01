a11d78424d51e56717608c46ce85bc868bda7f82
Mohammad Ali Sharpasand <m.a@sharpasand.com>
2016-09-01 12:24:48 +0430

    Write resume and fix responsive behavior.

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -1,11 +1,16 @@
                 <div id="summary">
                     <h3>R&eacute;sum&eacute;</h3>
-                    <p>Lorem ipsum dolor sit amet, no natum fierent vel, duo in nisl tamquam tibique. Usu ludus maluisset dissentias ad, vix vidisse qualisque voluptaria an. Magna option vel ea, an enim iudicabit nam. Movet disputationi id cum, sed oblique postulant signiferumque ei. Id eam quodsi diceret concludaturque, id ubique quaerendum mel. No ius populo iriure, ut dictas liberavisse est. Has ne sanctus eloquentiam. Harum nusquam quaestio id usu. His suas vero ne. Nam ut utinam accusamus, cum vitae aeterno fastidii in, ad mutat interpretaris has. Eu nec reque fabellas singulis, ea quo ubique ocurreret. Duis habemus consequat nec id, vide dicam verterem te qui. Impedit eligendi pri ne, qui ornatus nonumes dissentiet ei. Ad erat maiorum mea, ad cibo noluisse pri. Pro vero malorum id. At per tractatos interesset, an mazim aperiam nominavi sed. Cum ex harum offendit democritum. Vim assum urbanitas tincidunt id, eos at quot possim fabellas. Ea sea falli dolorem maiorum, eum ipsum prodesset constituam te, nam vide diceret imperdiet te. Duo nonumy ceteros platonem cu, qui labore eripuit eloquentiam ne, eu delicata contentiones sea. Per eu mucius possit interesset.</p>
-                </div>
-                <div id="resume-links">
-                    <ul>
-                        <li><a href="/resume">Short R&eacute;sum&eacute;</a></li>
-                        <li><a href="/ecv">Extended Curriculum Vita</a></li>
-                        <li><a href="http://ir.linkedin.com/pub/mohammad-ali-sharpasand/51/16/584">Linked In</a></li>
-                    </ul>
+                    <div id="resume-links">
+                        <ul>
+                            <li><a href="/resume">Short R&eacute;sum&eacute;</a></li>
+                            <li><a href="/ecv">Extended CV</a></li>
+                            <li><a href="http://ir.linkedin.com/pub/mohammad-ali-sharpasand/51/16/584">Linked In</a></li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <p>I am excited about research specially in the fields of Internet, Peer to Peer Networks, Bayesian Estimation, Machine Learning and Artificial Intelligence. </p>
+                        <p>After earning my Diploma in Mathematics from <a href="https://en.wikipedia.org/wiki/National_Organization_for_Development_of_Exceptional_Talents">Natinoal Organization for Development of Exceptional Tallents (NODET)</a> in Karaj Iran, I continued my education on Computer Engineering &ndash; Software in Qazvin Azad University, which is known for the best research opportunities on Robotics among Iranian universities. I joined <a href="http://mrl-spl.ir">Mechatronics Research Laboratory - Standard Platform League (MRL-SPL)</a> and earned undergraduate research scholarship. My B.Sc. Study is going to finish on July 2017.</p>
+                        <p>During my activity in MRL-SPL, I involved in and initated serveral research projects &mdash; selectively listed below &mdash; and won several competition/research awards in <a href="http://www.robocup.org/">RoboCup</a>. During this experience, I worked on Non-Parameteric Bayesian Estimation, Kalman Filtering, Monter Carlo Localization, Motion Planning, Kinematics Analysis, Simulation, Biped Locomotion, and Autonomus Agents. Besides, it provided me with a great teamwork experience which taught me Agile Software Development and Leadership and Organization Skills.</p>
+                    </div>
                 </div>
